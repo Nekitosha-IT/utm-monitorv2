ba7e3ee62cbf01155dcabf4b09fc0bcb977db066
Nekitosha-IT <mrvendettabest@gmail.com>
2026-09-10 11:38:41 +0300

    Fix UTM request service to use XML client

--- EGAISInspector.Core/Utm/UtmRequests.cs
+++ EGAISInspector.Core/Utm/UtmRequests.cs
@@ -1,11 +1,9 @@
-using System.Text;
 using System.Xml.Linq;
 namespace EGAISInspector.Core.Utm;
 public sealed class UtmRequests(UtmClient client)
 {
-    public Task<HttpResponseMessage> QueryFormAAsync(string fsrarId,string reg,CancellationToken ct=default)=>PostAsync("QueryFormA",EgaisXml.QueryFormA(fsrarId,reg),ct);
-    public Task<HttpResponseMessage> QueryFormBAsync(string fsrarId,string reg,CancellationToken ct=default)=>PostAsync("QueryFormB",EgaisXml.QueryFormB(fsrarId,reg),ct);
-    public Task<HttpResponseMessage> QueryFormBHistoryAsync(string fsrarId,string reg,CancellationToken ct=default)=>PostAsync("QueryFormBHistory",EgaisXml.QueryFormBHistory(fsrarId,reg),ct);
-    public Task<HttpResponseMessage> QueryRestsAsync(string fsrarId,CancellationToken ct=default)=>PostAsync("QueryRests",EgaisXml.QueryRests(fsrarId),ct);
-    async Task<HttpResponseMessage> PostAsync(string endpoint,XDocument xml,CancellationToken ct){using var content=new StringContent(xml.ToString(SaveOptions.DisableFormatting),Encoding.UTF8,"application/xml");return await client.PostAsync($"/opt/in/{endpoint}",content,ct);}
+    public Task<XDocument> QueryFormAAsync(string fsrarId,string reg,CancellationToken ct=default)=>client.PostXmlAsync("/opt/in/QueryFormA",EgaisXml.QueryFormA(fsrarId,reg),ct);
+    public Task<XDocument> QueryFormBAsync(string fsrarId,string reg,CancellationToken ct=default)=>client.PostXmlAsync("/opt/in/QueryFormB",EgaisXml.QueryFormB(fsrarId,reg),ct);
+    public Task<XDocument> QueryFormBHistoryAsync(string fsrarId,string reg,CancellationToken ct=default)=>client.PostXmlAsync("/opt/in/QueryFormBHistory",EgaisXml.QueryFormBHistory(fsrarId,reg),ct);
+    public Task<XDocument> QueryRestsAsync(string fsrarId,CancellationToken ct=default)=>client.PostXmlAsync("/opt/in/QueryRests",EgaisXml.QueryRests(fsrarId),ct);
 }
