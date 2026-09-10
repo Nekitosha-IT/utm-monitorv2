cfc61a3de717ea36cb0802857cce91e89f2bf6d7
Nekitosha-IT <mrvendettabest@gmail.com>
2026-09-10 11:29:11 +0300

    feat: add main window code-behind

---
+++ src/EGAISInspector/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+using Wpf.Ui.Controls;
+
+namespace EGAISInspector;
+
+public partial class MainWindow : FluentWindow
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
